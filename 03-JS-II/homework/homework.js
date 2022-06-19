d8d418407c7a3bf96718ab1e54cd264330a56430
Arcijesus <magdalenoarci@gmail.com>
2022-06-19 02:11:46 -0300

    solucion 6

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -169,9 +169,12 @@ function esVerdadero(valor){
 function tablaDelSeis(){
   //Escribe una función que muestre la tabla de multiplicar del 6 (del 0 al 60).
   //La función devuelve un array con los resultados de la tabla de multiplicar del 6 en orden creciente.
-  //Escribe tu código aquí   
-  
+  var seis = []
+  for( var i =0; i<61 ; i +=6) {
+    seis.push(i);
+  } return seis;
 }
+  
 
 function tieneTresDigitos(numero){
   //Leer un número entero y retornar true si tiene 3 dígitos. Caso contrario, retorna false.
